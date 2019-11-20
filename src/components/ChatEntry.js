098effb5ace3d00e9b1039ab079f30f572d81cde
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-11-20 15:20:21 -0800

    added ChatEntry component. no clue if it will render

---
+++ src/components/ChatEntry.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ChatEntry.css';
+import Timestamp from './Timestamp';
+
+const ChatEntry = (props) => {
+  return(
+    <section>
+      <p>{props.message.sender}</p>
+      <p>
+        {props.message.body}
+        <Timestamp time={props.message.timeStamp} />
+      </p>
+    </section>
+  );
+};
+
+export default ChatEntry;
