6a899a94792f63e881a41f8fcfde16a3bb3cde70
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-11-20 16:02:14 -0800

    dynamic app title

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ import ChatLog from './components/ChatLog';
 const App = () => {
   console.log(chatMessages);
 
+  const participants = chatMessages.map((message, i) => {
+    return message.sender;
+  });
+
+  // new Set returns only unique elements of an array
+  const uniqueParticipants = [...new Set(participants)];
+  let nameStr = uniqueParticipants[0];
+
+  for (let i=1; i < uniqueParticipants.length; i+=1) {
+    if (i === uniqueParticipants.length - 1) {
+      nameStr += ` and ${uniqueParticipants[i]}`;
+    } else {
+      nameStr += `, ${uniqueParticipants[i]}`;
+    }
+  }
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between {nameStr}</h1>
       </header>
       <main>
         <ChatLog messages={chatMessages} />
